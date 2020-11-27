27d81e5512c4eb50264aa63b56a393966046791c
matthiaskarg <matthias.karg88@googlemail.com>
2020-11-27 16:08:33 +0100

    add chapter 7 braces

---
+++ 07_braces/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class A
+{
+    int j{0};
+    int i = 0;
+
+};
+
+// use () always
+// and {} just for std::vector a{1,2,3,4};
+
+int main()
+{
+    // initializing:
+    int i = 0;
+    int j (0);
+    int u {0};
+
+    return 0;
+}
